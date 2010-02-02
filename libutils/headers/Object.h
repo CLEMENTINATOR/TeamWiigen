e23bbb0f35c9738b61e6e6a517276e683e6eba69
arasium <arasium@e6d814ab-5cd9-41c0-8def-334f96fd17b7>
2010-02-02 15:55:35 +0000

    encore des petite modif d'analyses

--- libutils/headers/Object.h
+++ libutils/headers/Object.h
@@ -12,7 +12,7 @@
 mi1 = mallinfo();\
 std::cout << "used space: " << mi1.uordblks + mi1.usmblks << std::endl
 
-#define M2(x) mi1 = mi2; mi2 = mallinfo();\
+#define M2(x) mi2 = mallinfo();\
 cout << std::endl << (x) << " consumed " << mi2.uordblks + mi2.usmblks - mi1.uordblks - mi1.usmblks << std::endl \
 			  << "used space: " << mi2.uordblks + mi2.usmblks << std::endl
 

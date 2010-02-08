d8933685a4028b459fd6e8d01f7f9a1a2624fecf
arasium <arasium@e6d814ab-5cd9-41c0-8def-334f96fd17b7>
2010-02-08 13:43:20 +0000

    pb de conflit

--- sciifii/source/main.cpp
+++ sciifii/source/main.cpp
@@ -63,35 +63,21 @@ int main(int argc, char **argv)
   Config::CreateUpdateList(uninstall);
 
   Disclaimer::Show();
-<<<<<<< .mine
   WPAD_Shutdown();
   
-=======
-
->>>>>>> .r32
   try
   {
 	  Sciifii sci;
 	  Device::Mount("sd:/");
-<<<<<<< .mine
-	  if(sci.Prepare())
-=======
       WPAD_Shutdown();
-	  if(!sci.Prepare())
->>>>>>> .r32
+	  if(sci.Prepare())
 	  {
 	    sci.Execute();
 	    Device::UnMount("sd:/");
 	    cout << "Installation done! Press A to reboot the wii.";
 	  }
-<<<<<<< .mine
 	  else
 	    cout << "An error occured. Press A to reboot the wii.";
-=======
-	  sci.Execute();
-	  Device::UnMount("sd:/");
-	  cout << "Installation done! Press A to reboot the wii.";
->>>>>>> .r32
   }
   catch(Exception &ex)
   {

d98d5beea4dc431431641eaf6ed9e138cab3c975
Clément B <CLEMENTINATOR@users.noreply.github.com>
2010-02-20 20:45:44 +0000

    ajout try catch main, ajout Exception si fichier config.xml non existant

--- sciifii/source/main.cpp
+++ sciifii/source/main.cpp
@@ -22,76 +22,87 @@ static GXRModeObj *vmode;
 
 int main(int argc, char **argv)
 {
-  VIDEO_Init();
+    VIDEO_Init();
 
-  vmode = VIDEO_GetPreferredMode(NULL);
-  xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(vmode));
+    vmode = VIDEO_GetPreferredMode(NULL);
+    xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(vmode));
 
-  VIDEO_Configure(vmode);
-  VIDEO_SetNextFramebuffer(xfb);
-  VIDEO_SetBlack(false);
-  VIDEO_Flush();
+    VIDEO_Configure(vmode);
+    VIDEO_SetNextFramebuffer(xfb);
+    VIDEO_SetBlack(false);
+    VIDEO_Flush();
 
-  VIDEO_WaitVSync();
-  if (vmode->viTVMode & VI_NON_INTERLACE) VIDEO_WaitVSync();
+    VIDEO_WaitVSync();
+    if (vmode->viTVMode & VI_NON_INTERLACE) VIDEO_WaitVSync();
 
-  int x = 20, y = 20, w, h;
-  w = vmode->fbWidth - (x * 2);
-  h = vmode->xfbHeight - (y + 20);
+    int x = 20, y = 20, w, h;
+    w = vmode->fbWidth - (x * 2);
+    h = vmode->xfbHeight - (y + 20);
 
-  // Initialize the console
-  CON_InitEx(vmode, x, y, w, h);
-  VIDEO_ClearFrameBuffer(vmode, xfb, COLOR_BLACK);
+    // Initialize the console
+    CON_InitEx(vmode, x, y, w, h);
+    VIDEO_ClearFrameBuffer(vmode, xfb, COLOR_BLACK);
 
-  WPAD_Init();
+    WPAD_Init();
 
-  Device::Mount("sd:/");
-  Config::Initialize();
+    Device::Mount("sd:/");
+    try
+    {
+        Config::Initialize();
+    }
+    catch (Exception &ex)
+    {
+        cout << endl << "\x1b[33m" << "Exception " << ex.GetCode() << endl
+        << ex.GetMessage() << "\x1b[37m" << endl
+        << "Press A to reboot the wii and relaunch sciifii.";
+        Pause();
+        STM_RebootSystem();
+    }
 
-  MainMenu menu;
-  MainMenuResult result = menu.Show();
+    MainMenu menu;
+    MainMenuResult result = menu.Show();
 
-  if(result == mmResult_Exit)
-		  STM_RebootSystem();
+    if (result == mmResult_Exit)
+        STM_RebootSystem();
 
-  if(result == mmResult_Advanced)
-  {
-	AdvancedMenu aMenu;
-	AdvancedMenuResult aresult = aMenu.Show();
-	if(aresult == amResult_Exit)
-		  STM_RebootSystem();
-  }
+    if (result == mmResult_Advanced)
+    {
+        AdvancedMenu aMenu;
+        AdvancedMenuResult aresult = aMenu.Show();
+        if (aresult == amResult_Exit)
+            STM_RebootSystem();
+    }
 
-  Config::ValidateOptions();
+    Config::ValidateOptions();
 
-  Disclaimer::Show();
-  WPAD_Shutdown();
+    Disclaimer::Show();
+    WPAD_Shutdown();
 
-  try
-  {
-	  Sciifii sci;
-      WPAD_Shutdown();
-	  if(sci.Prepare())
-	  {
-	    sci.Execute();
-	    Device::UnMount("sd:/");
-	    cout << "Installation done! Press A to reboot the wii.";
-	  }
-	  else
-	    cout << "An error occured. Press A to reboot the wii.";
-  }
-  catch(Exception &ex)
-  {
-	cout << endl << "\x1b[33m" << "Exception " << ex.GetCode() << endl
-		 << ex.GetMessage() << "\x1b[37m" << endl
-		 << "Press A to reboot the wii and relaunch sciifii.";
-  }
-  catch(...)
-  {
-	cout << "Unexpected Exception!" << endl
-		 << "Press A to reboot the wii and relaunch sciifii.";
-  }
-  WPAD_Init();
-  Pause();
-  STM_RebootSystem();
+    try
+    {
+        Sciifii sci;
+        WPAD_Shutdown();
+        if (sci.Prepare())
+        {
+            sci.Execute();
+            Device::UnMount("sd:/");
+            cout << "Installation done! Press A to reboot the wii.";
+        }
+        else
+            cout << "An error occured. Press A to reboot the wii.";
+    }
+    catch (Exception &ex)
+    {
+        cout << endl << "\x1b[33m" << "Exception " << ex.GetCode() << endl
+        << ex.GetMessage() << "\x1b[37m" << endl
+        << "Press A to reboot the wii and relaunch sciifii.";
+    }
+    catch (...)
+    {
+        cout << "Unexpected Exception!" << endl
+        << "Press A to reboot the wii and relaunch sciifii.";
+    }
+    WPAD_Init();
+    Pause();
+    STM_RebootSystem();
 }

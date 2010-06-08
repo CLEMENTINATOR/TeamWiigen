234741eb78d31be667d6741134cceeb2bcf6a79d
Clément B <CLEMENTINATOR@users.noreply.github.com>
2010-06-08 19:57:59 +0000

    Doc.

--- libutils/source/logging/Log.cpp
+++ libutils/source/logging/Log.cpp
@@ -1,26 +1,39 @@
 #include <logging/Log.h>
 
 using namespace std;
-
+/**
+ * \brief Constructor
+ * Initialize log system
+ */
 Log::Log()
 {
 	_logs.insert(make_pair(Log_Error, new vector<ILogProvider*>()));
 	_logs.insert(make_pair(Log_Warning, new vector<ILogProvider*>()));
 	_logs.insert(make_pair(Log_Info, new vector<ILogProvider*>()));
 }
-
+/**
+ *\brief Destructor
+ */
 Log::~Log()
 {
 	for(map<LogStatus, vector<ILogProvider*>*>::iterator ite = _logs.begin(); ite != _logs.end(); ite++)
 		delete ite->second;
 }
-
+/**
+ * \brief Return the current log system
+ * \return The log system
+ */
 Log& Log::Current()
 {
 	static Log l;
 	return l;
 }
-
+/**
+ *\brief Add a log provider to the log system
+ *\param status The LogStatus associed to the provider
+ *\param logger The Log provider ( Could be FileLogger or GekkoLogger)
+ *\see LogStatus
+ */
 void Log::AddLogProvider(LogStatus status, ILogProvider* logger)
 {
 	if((status & Log_Error) == Log_Error)
@@ -32,7 +45,13 @@ void Log::AddLogProvider(LogStatus status, ILogProvider* logger)
 	if((status & Log_Info) == Log_Info)
 		Current()._logs.find(Log_Info)->second->push_back(logger);
 }
-
+/**
+ *\brief Write to logs
+ *\param status The LogStatus associated to the message to write
+ *\param source
+ *\param message The message to write
+ *\param code The message code(could be an error code etc ..)
+ */
 void Log::Write(LogStatus status, const std::string& source, const std::string& message, s32 code)
 {
 	for(map<LogStatus, vector<ILogProvider*>*>::iterator ite = Current()._logs.begin(); ite != Current()._logs.end(); ite++)

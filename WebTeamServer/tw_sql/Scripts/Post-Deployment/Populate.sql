43de48a9390dca7dc2b0304c5b09b1a1b344a4c8
arasium <arasium@e6d814ab-5cd9-41c0-8def-334f96fd17b7>
2010-06-15 20:55:39 +0000

    Clean some config

--- WebTeamServer/tw_sql/Scripts/Post-Deployment/Populate.sql
+++ WebTeamServer/tw_sql/Scripts/Post-Deployment/Populate.sql
@@ -1,6 +1,9 @@
 ﻿-- =============================================
 -- Script Template
 -- =============================================
+IF NOT EXISTS(SELECT TOP(1) 1 FROM LOG_TYPE)
+BEGIN
 INSERT INTO LOG_TYPE(LTY_CODE, LTY_VALUE) VALUES('ERR', 'error')
 INSERT INTO LOG_TYPE(LTY_CODE, LTY_VALUE) VALUES('WAR', 'warning')
 INSERT INTO LOG_TYPE(LTY_CODE, LTY_VALUE) VALUES('NFO', 'info')
+END

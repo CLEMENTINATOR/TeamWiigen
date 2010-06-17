5433b67275607407d2a95f190eeb072822587a3a
SkinnerDev <SkinnerDev@e6d814ab-5cd9-41c0-8def-334f96fd17b7>
2010-06-17 22:20:19 +0000

    New form a little bit better but not work yet!

---
+++ Sciifii Downloader/Sciifii/MainForm.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Sciifii
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.lbMode = new System.Windows.Forms.ListBox();
+            this.sciifiiModeBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tbStatus = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.clbOption = new System.Windows.Forms.CheckedListBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.pbStatus = new System.Windows.Forms.ToolStripProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.ddlRegion = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.sciifiiModeBindingSource)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Fichier config.xml|config.xml";
+            // 
+            // lbMode
+            // 
+            this.lbMode.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)));
+            this.lbMode.DataSource = this.sciifiiModeBindingSource;
+            this.lbMode.DisplayMember = "Text";
+            this.lbMode.FormattingEnabled = true;
+            this.lbMode.Location = new System.Drawing.Point(6, 27);
+            this.lbMode.Name = "lbMode";
+            this.lbMode.Size = new System.Drawing.Size(188, 238);
+            this.lbMode.TabIndex = 0;
+            this.lbMode.ValueMember = "options";
+            this.lbMode.SelectedIndexChanged += new System.EventHandler(this.lbMode_SelectedIndexChanged);
+            // 
+            // sciifiiModeBindingSource
+            // 
+            this.sciifiiModeBindingSource.DataSource = typeof(SciifiiDTO.Mode);
+            // 
+            // tbStatus
+            // 
+            this.tbStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbStatus.BackColor = System.Drawing.SystemColors.WindowText;
+            this.tbStatus.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbStatus.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.tbStatus.Location = new System.Drawing.Point(424, 36);
+            this.tbStatus.Multiline = true;
+            this.tbStatus.Name = "tbStatus";
+            this.tbStatus.Size = new System.Drawing.Size(200, 244);
+            this.tbStatus.TabIndex = 10;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox1.Controls.Add(this.lbMode);
+            this.groupBox1.Controls.Add(this.clbOption);
+            this.groupBox1.Location = new System.Drawing.Point(12, 9);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(406, 271);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select mode:";
+            // 
+            // clbOption
+            // 
+            this.clbOption.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)));
+            this.clbOption.BackColor = System.Drawing.SystemColors.Control;
+            this.clbOption.CheckOnClick = true;
+            this.clbOption.FormattingEnabled = true;
+            this.clbOption.Location = new System.Drawing.Point(200, 21);
+            this.clbOption.Name = "clbOption";
+            this.clbOption.Size = new System.Drawing.Size(200, 244);
+            this.clbOption.TabIndex = 1;
+            this.clbOption.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.clbOption_ItemCheck);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.pbStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 290);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(636, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(55, 17);
+            this.toolStripStatusLabel1.Text = "Progress:";
+            // 
+            // pbStatus
+            // 
+            this.pbStatus.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.pbStatus.Name = "pbStatus";
+            this.pbStatus.Size = new System.Drawing.Size(525, 16);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDownload.Location = new System.Drawing.Point(549, 7);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(75, 23);
+            this.btnDownload.TabIndex = 3;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // ddlRegion
+            // 
+            this.ddlRegion.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ddlRegion.FormattingEnabled = true;
+            this.ddlRegion.Location = new System.Drawing.Point(424, 9);
+            this.ddlRegion.Name = "ddlRegion";
+            this.ddlRegion.Size = new System.Drawing.Size(49, 21);
+            this.ddlRegion.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 312);
+            this.Controls.Add(this.ddlRegion);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.tbStatus);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(652, 330);
+            this.Name = "MainForm";
+            this.Text = "Sciifii downloader";
+            ((System.ComponentModel.ISupportInitialize)(this.sciifiiModeBindingSource)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox lbMode;
+        private System.Windows.Forms.TextBox tbStatus;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckedListBox clbOption;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripProgressBar pbStatus;
+        private System.Windows.Forms.BindingSource sciifiiModeBindingSource;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.ComboBox ddlRegion;
+    }
+}
---
+++ Sciifii Downloader/Sciifii/MainForm.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using SciifiiDTO;
+using System.Xml.Serialization;
+
+namespace Sciifii
+{
+    public partial class MainForm : Form
+    {
+        #region private
+        private SciifiiConfiguration datas;
+        private bool blockList = false;
+        private bool job = false;
+        private string directory;
+        private List<String> hiddenOptions;
+
+        private void StopJob()
+        {
+            m_UpTextBox(tbStatus, "Cancel in progress");
+            backgroundWorker1.CancelAsync();
+        }
+
+        private void StartJob()
+        {
+            m_UpTextBox(tbStatus, "Download started");
+            List<string> options = new List<string>();
+            List<Step> steps = new List<Step>();
+
+            foreach (Option option in clbOption.CheckedItems)
+                options.Add(option.Name);
+
+            options.AddRange(hiddenOptions);
+
+            foreach (Step s in datas.Steps)
+            {
+                foreach (string sopt in s.Options)
+                    if (options.Contains(sopt))
+                    {
+                        steps.Add(s);
+                        break;
+                    }
+            }
+
+            backgroundWorker1.RunWorkerAsync(steps.ToArray());
+        }
+        #endregion
+
+        #region public
+        public MainForm()
+        {
+            InitializeComponent();
+
+            //Register delegate to update textbox with thread
+            m_UpTextBox = new UpdateTextBoxDelegate(this.UpdateTextBox);
+
+            tbStatus.Text = "";
+            hiddenOptions = new List<string>();
+
+            clbOption.DisplayMember = "text";
+
+            String fileName = "config.xml";
+            //Load config.xml
+            if (!File.Exists(fileName))
+            {
+                MessageBox.Show("File config.xml not found", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                    fileName = openFileDialog1.FileName;
+                else
+                {
+                    MessageBox.Show("config.xml is expect application will close now", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.Close();
+                }
+            }
+
+            using (Stream config = File.Open(fileName, FileMode.Open, FileAccess.Read))
+                datas = (SciifiiConfiguration)new XmlSerializer(typeof(SciifiiConfiguration)).Deserialize(config);
+
+            m_UpTextBox(tbStatus, "Config.xml load with success.");
+
+            List<Mode> modes = new List<Mode>();
+            modes.Add(new Mode { Text = "Advanced mode", OptionsString = "", Flag = "" });
+            modes.AddRange(datas.Modes);
+
+            sciifiiModeBindingSource.DataSource = modes;
+
+            clbOption.Items.Clear();
+            foreach (Option option in datas.Options)
+                if (!option.Hidden)
+                    clbOption.Items.Add(option, false);
+
+            ddlRegion.DataSource = Enum.GetNames(typeof(Regions)).Where(x => x != "ALL").OrderBy(x => x).ToList();        
+        }
+        #endregion
+
+        #region events
+        protected void lbMode_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (blockList)
+                return;
+
+            blockList = true;
+
+            hiddenOptions.Clear();
+            List<string> options = lbMode.SelectedValue as List<string>;
+
+            clbOption.Items.Clear();
+            if (options != null)
+                foreach (Option option in datas.Options)
+                    if (option.Hidden)
+                    {
+                        if (options.Contains(option.Name))
+                            hiddenOptions.Add(option.Name);
+                    }
+                    else
+                        clbOption.Items.Add(option, options.Contains(option.Name));
+
+            blockList = false;
+        }
+
+        protected void clbOption_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            if (blockList)
+                return;
+
+            blockList = true;
+            hiddenOptions.Clear();
+            lbMode.SelectedIndex = 0;
+            blockList = false;
+        }
+
+        protected void btnDownload_Click(object sender, EventArgs e)
+        {
+            if (!job)
+            {
+                FolderBrowserDialog dial = new FolderBrowserDialog();
+                if (dial.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    datas.SelectedRegion = (Regions)Enum.Parse(typeof(Regions), ddlRegion.SelectedValue.ToString());
+                    directory = dial.SelectedPath;
+                    StartJob();
+                }
+            }
+            else
+                StopJob();
+        }
+
+        protected void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            this.Invoke(new MethodInvoker(delegate()
+            {
+                btnDownload.Text = "Cancel";
+            }));
+            job = true;
+            Downloader d = new Downloader(e, directory, datas, (BackgroundWorker)sender);
+            d.ExecuteDownload();
+        }
+
+        protected void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            pbStatus.Value = e.ProgressPercentage;
+            m_UpTextBox(tbStatus, "");
+        }
+
+        protected void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            m_UpTextBox(tbStatus, "Download finish");
+            pbStatus.Value = 0;
+            
+            if (e.Error != null)
+               m_UpTextBox(tbStatus, "An error occured;\n" + e.Error.Message);
+
+            this.Invoke(new MethodInvoker(delegate()
+            {
+                btnDownload.Text = "Download";
+            }));
+            job = false;
+        }
+        #endregion
+
+        #region ControlManagment
+        private delegate void UpdateTextBoxDelegate(System.Windows.Forms.TextBox tb, String value);
+        private UpdateTextBoxDelegate m_UpTextBox;
+
+        private void UpdateTextBox(System.Windows.Forms.TextBox tb, String value)
+        {
+            value = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss") + " : " + value;
+            // Small function for writing text to the statusbox...
+            if (tb.Text == "")
+                tb.Text = value;
+            else
+                tb.Text += "\r\n" + value;
+
+            // Scroll to end of text box.
+            tb.SelectionStart = tb.TextLength;
+            tb.ScrollToCaret();
+        }
+
+        private void ModifTextBox(System.Windows.Forms.TextBox tb, String value)
+        {
+            object[] arg = { tb, value };
+            tb.Invoke(m_UpTextBox, arg);
+        }
+        #endregion
+    }
+}

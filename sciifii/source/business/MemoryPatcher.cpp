17b57cc2781507d309c151f1139e24bff0ce10d3
arasium <arasium@e6d814ab-5cd9-41c0-8def-334f96fd17b7>
2010-09-16 14:13:58 +0000

    pb au cios sur le 36

--- sciifii/source/business/MemoryPatcher.cpp
+++ sciifii/source/business/MemoryPatcher.cpp
@@ -1,9 +1,10 @@
 #include "MemoryPatcher.h"
 #include <libwiisys.h>
 #include <gccore.h>
+#include <ogc/machine/processor.h>
 
 #define HAVE_AHBPROT (*(vu32*)0xcd800064 == 0xFFFFFFFF)
-#define MEM2_PROT (*(u32*)0xd8b420A)
+#define MEM2_PROT 0xd8b420A
 #define IOS_MEM_ADDRESS 0x93400000
 #define IOS_MEM_ENDADDRESS 0x94000000
 
@@ -31,8 +32,8 @@ void MemoryPatcher::Install()
 	if(!HAVE_AHBPROT)
 		throw Exception("Use HBC 1.0.7 or higher and don't reload the IOS in order to have the AHBPROT disabled.", -1);
 
-	u32 mem2Status = MEM2_PROT;
-	MEM2_PROT =  mem2Status & 0x0000FFFF;
+	u32 mem2Status = read32(MEM2_PROT);
+	write32(MEM2_PROT, mem2Status & 0x0000FFFF);
 
 	for(vector<MemoryPatch>::iterator ite = _patchList.begin(); ite != _patchList.end(); ite++)
 	{
@@ -42,13 +43,13 @@ void MemoryPatcher::Install()
 			if (!memcmp(ptr, ite->pattern.Content(), ite->pattern.Length()))
 			{
 				memcpy(ptr, ite->patch.Content(), ite->patch.Length());
-				DCFlushRange(TITLE_ROUND_DOWN(ptr, 32), TITLE_ROUND_UP(ite->patch.Length(), 32) + 32);
+				DCFlushRange((u8*)TITLE_ROUND_DOWN((u32)ptr, 32), TITLE_ROUND_UP(ite->patch.Length(), 32) + 32);
 			}
 			ptr++;
 		}
 	}
 
-	MEM2_PROT = mem2Status;
+	write32(MEM2_PROT, mem2Status);
 	OnProgress("Running IOS patched.", 1);
 }
 

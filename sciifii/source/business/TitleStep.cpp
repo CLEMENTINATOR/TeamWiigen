62af197f254ae28b4144e8b39fe7c12e4420f450
Clément B <CLEMENTINATOR@users.noreply.github.com>
2010-10-14 08:36:58 +0000

    fix update

--- sciifii/source/business/TitleStep.cpp
+++ sciifii/source/business/TitleStep.cpp
@@ -13,16 +13,18 @@ using namespace Libwiisys::IO;
 using namespace Libwiisys::System::Event;
 using namespace Libwiisys;
 
-
 TitleStep::TitleStep(u64 titleId, u16 revision, TitleAction a, string path) :
-	_id(titleId), _revision(revision), _action(a), _file(""), _path(path) {
+	_id(titleId), _revision(revision), _action(a), _file(""), _path(path)
+{
 }
 
 TitleStep::TitleStep(string file, TitleAction a, string path) :
-	_id(0), _revision(0), _action(a), _file(file), _path(path) {
+	_id(0), _revision(0), _action(a), _file(file), _path(path)
+{
 }
 
-bool TitleStep::Prepare() {
+bool TitleStep::Prepare()
+{
 	if (_action == ti_PackAsWad && Path::GetFileExtension(_path) != "wad")
 		throw Exception(
 				"You must specify a wad file path in order to store the wad");
@@ -40,92 +42,100 @@ bool TitleStep::Prepare() {
 	if (_action == ti_Extract && _file != "")
 		throw Exception("This is impossible to extract a title from a wad!");
 
-	if (_action == ti_Update) {
-		if	(!_revision)
-		{
-			if(_revision<=Title::GetInstalledTitleVersion(_id))
-			{
-				OnProgress("Title update preparation done!", 1);
-				return true;
-			}
-
-		}
-
-	}
-
-if (_file!="") /* Si fichier donne en parametre */
-{
-	OnProgress("Getting wad file", 0.25);
-	if (!FileManager::Download(_file))
-	throw Exception("Error downloading " + _file);
-	_file = FileManager::GetPath(_file);
-}
-else /*  Si tid */
-{
-	if (_action!=ti_Uninstall&& _action!=ti_Extract)
+	if (_file != "") /* Si fichier donne en parametre */
 	{
-		stringstream wad;
-		wad << Config::WorkingDirectory() << "/" << Title::GetWadFormatedName(_id,_revision);
-		_file = wad.str();
+		OnProgress("Getting wad file", 0.25);
+		if (!FileManager::Download(_file))
+			throw Exception("Error downloading " + _file);
+		_file = FileManager::GetPath(_file);
+	}
+	else /*  Si tid */
+	{
 
-		if (!File::Exists(_file))
+		if (_action == ti_Update)
 		{
-			if (Config::HasNetwork())
+			if (_revision != 0)
 			{
-				Title ios;
+				if (_revision > Title::GetInstalledTitleVersion(_id))
+				{
+					_action = ti_Install;
+				}
+				else
+				{ // no need for update
+					OnProgress("Title update not necessary !", 1);
+					return true;
+				}
 
-				stringstream down;
-				down << "Downloading title " << hex << setfill('0') << setw(16) << _id << dec;
-				if ( _revision!=0)
-				down << " version " << _revision;
-				down << " from NUS.";
-
-				OnProgress(down.str(), 0.25);
-				ios.LoadFromNusServer(_id, _revision, Config::WorkingDirectory());
-
-				stringstream pack;
-				pack << "Saving as " << Path::GetFileName(_file);
-				OnProgress(pack.str(), 0.75);
-				ios.PackAsWad(_file);
 			}
-			else
+
+		}
+		if (_action != ti_Uninstall && _action != ti_Extract)
+		{
+			stringstream wad;
+			wad << Config::WorkingDirectory() << "/"
+					<< Title::GetWadFormatedName(_id, _revision);
+			_file = wad.str();
+
+			if (!File::Exists(_file))
 			{
-				cout << "You arent connected to the network and some wads are missing." << endl << "Please refer to the readme.";
-				return false;
+				if (Config::HasNetwork())
+				{
+					Title ios;
+
+					stringstream down;
+					down << "Downloading title " << hex << setfill('0')
+							<< setw(16) << _id << dec;
+					if (_revision != 0)
+						down << " version " << _revision;
+					down << " from NUS.";
+
+					OnProgress(down.str(), 0.25);
+					ios.LoadFromNusServer(_id, _revision,
+							Config::WorkingDirectory());
+
+					stringstream pack;
+					pack << "Saving as " << Path::GetFileName(_file);
+					OnProgress(pack.str(), 0.75);
+					ios.PackAsWad(_file);
+				}
+				else
+				{
+					cout
+							<< "You arent connected to the network and some wads are missing."
+							<< endl << "Please refer to the readme.";
+					return false;
+				}
 			}
 		}
 	}
+
+	if (_action == ti_Uninstall)
+		OnProgress("Title uninstallation preparation done!", 1);
+	else if (_action == ti_Install)
+		OnProgress("Title installation preparation done!", 1);
+	else if (_action == ti_PackAsWad)
+		OnProgress("Wad creation preparation done!", 1);
+
+	return true;
 }
 
-if (_action == ti_Uninstall)
-OnProgress("Title uninstallation preparation done!", 1);
-else if (_action == ti_Install)
-OnProgress("Title installation preparation done!", 1);
-else if (_action == ti_PackAsWad)
-OnProgress("Wad creation preparation done!", 1);
-
-return true;
-}
-
-void TitleStep::Install() {
+void TitleStep::Install()
+{
 	stringstream str;
 
-	if (_action == ti_Update) {
-		if	(!_revision)
-		{
-			if(_revision<=Title::GetInstalledTitleVersion(_id))
-			{
-				OnProgress("Title update install done!", 1);
-				return ;
-			}
-		}
+	if (_action == ti_Update)
+	{
+		OnProgress("Title update not necessary !", 1);
 	}
-	if (_action == ti_Uninstall && _id != 0) {
+	if (_action == ti_Uninstall && _id != 0)
+	{
 		str << "Uninstalling title " << hex << setfill('0') << setw(16) << _id
 				<< dec;
 		OnProgress(str.str(), 0.25);
 		Title::Uninstall(_id);
-	} else if (_action == ti_Uninstall && _id == 0) {
+	}
+	else if (_action == ti_Uninstall && _id == 0)
+	{
 		Title t;
 		str << "Loading title from " << _file;
 		OnProgress(str.str(), 0.25);
@@ -134,7 +144,9 @@ void TitleStep::Install() {
 		str2 << "Uninstalling title " << _file;
 		OnProgress(str2.str(), 0.75);
 		t.Uninstall();
-	} else if (_action == ti_Install) {
+	}
+	else if (_action == ti_Install)
+	{
 		Title t;
 		if (_id == 0)
 			str << "Loading title from " << _file;
@@ -151,7 +163,9 @@ void TitleStep::Install() {
 					<< _id << dec;
 		OnProgress(str2.str(), 0.75);
 		t.Install();
-	} else if (_action == ti_PackAsWad) {
+	}
+	else if (_action == ti_PackAsWad)
+	{
 		if (Path::GetParentDirectory(_path) != "")
 			Directory::Create(Path::GetParentDirectory(_path));
 
@@ -159,7 +173,9 @@ void TitleStep::Install() {
 		str2 << "Moving  " << _file << " to " << _path;
 		OnProgress(str2.str(), 0.50);
 		File::Move(_file, _path);
-	} else if (_action == ti_Extract) {
+	}
+	else if (_action == ti_Extract)
+	{
 		Title t;
 		str << "Extracting title  " << hex << setfill('0') << setw(16) << _id
 				<< dec;
@@ -171,7 +187,9 @@ void TitleStep::Install() {
 				<< dec;
 		OnProgress(str2.str(), 0.75);
 		t.PackAsWad(_path);
-	} else if (_action == ti_Decrypt) {
+	}
+	else if (_action == ti_Decrypt)
+	{
 		Title t;
 		if (_id == 0)
 			str << "Loading title from " << _file;
@@ -202,7 +220,8 @@ void TitleStep::Install() {
 		OnProgress("Title extracted!", 1);
 }
 
-void TitleStep::SendtoLog() {
+void TitleStep::SendtoLog()
+{
 	stringstream str;
 	str << "TitleStep(" << hex << _id << dec << "," << _revision << ","
 			<< _action << "," << _file << "," << _path << ")";

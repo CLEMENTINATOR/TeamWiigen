10d49fe9048cdcfac51b720ebc76069b881b1610
Clément B <CLEMENTINATOR@users.noreply.github.com>
2010-10-21 14:14:38 +0000

    Indentation plugin

--- libwui/headers/Libwui/Component/ProgressBar.h
+++ libwui/headers/Libwui/Component/ProgressBar.h
@@ -7,33 +7,33 @@
 
 namespace Libwui
 {
-namespace Component
-{
-class ProgressBar: public Libwui::Control
-{
-public:
-	ProgressBar();
-	void SetText(std::string s);
-	void InitializeComponents();
-	void SetMaxValue(f32 max);
-	void SetActualValue(f32 act);
-	void SetValue(Libwiisys::Object *sender,Libwiisys::Threading::ProgressEventArgs *p);
-	void ProcessMessage(Message& message);
-	void SetUsedColor(GXColor c);
-	void SetDefaultColor(GXColor c);
-	void ForeColor(GXColor c);
-	void TextSize(int pt);
-	void SetTextAlignement(HAlign halign, VAlign valign);
-private:
-	f32 MaxValue;
-	f32 ActualValue;
-	Label l;
-	GXColor usedColor;
+  namespace Component
+  {
+    class ProgressBar: public Libwui::Control
+    {
+      public:
+        ProgressBar();
+        void SetText(std::string s);
+        void InitializeComponents();
+        void SetMaxValue(f32 max);
+        void SetActualValue(f32 act);
+        void SetValue(Libwiisys::Object *sender,Libwiisys::Threading::ProgressEventArgs *p);
+        void ProcessMessage(Message& message);
+        void SetUsedColor(GXColor c);
+        void SetDefaultColor(GXColor c);
+        void ForeColor(GXColor c);
+        void TextSize(int pt);
+        void SetTextAlignement(HAlign halign, VAlign valign);
+      private:
+        f32 MaxValue;
+        f32 ActualValue;
+        Label l;
+        GXColor usedColor;
 
-	GXColor defaultColor;
-protected:
-	void Draw();
-};
-}
+        GXColor defaultColor;
+      protected:
+        void Draw();
+    };
+  }
 }
 #endif /* PROGRESSBAR_H_ */
--- libwui/source/Component/ProgressBar.cpp
+++ libwui/source/Component/ProgressBar.cpp
@@ -13,154 +13,155 @@ using namespace Libwiisys::Threading;
 using namespace std;
 
 ProgressBar::ProgressBar() :
-	MaxValue(1),
-	ActualValue(0), 
-	l("", 12, Colors::Black()),
-	usedColor(Colors::Green()),
-	defaultColor(Colors::White())
+    MaxValue(1),
+    ActualValue(0),
+    l("", 12, Colors::Black()),
+    usedColor(Colors::Green()),
+    defaultColor(Colors::White())
 
 {
-BackgroundColor(Colors::Black());
+  BackgroundColor(Colors::Black());
 }
 
 void ProgressBar::InitializeComponents()
 {
-	l.SetTextAlignment(HAlign_Center, VAlign_Middle);
-	AddChildren(&l);
-	Control::InitializeComponents();
+  l.SetTextAlignment(HAlign_Center, VAlign_Middle);
+  AddChildren(&l);
+  Control::InitializeComponents();
 }
 
 void ProgressBar::SetMaxValue(f32 max)
 {
-	if (InvokeRequired())
-	{
-		stringstream params;
-		params << max;
-		Message* m = new Message(_fullId, "MaxValue", params.str());
-		UIManager::AddMessage(m);
-	}
-	else
-	{
-		if (max > 0)
-			MaxValue = max;
-	}
+  if (InvokeRequired())
+  {
+    stringstream params;
+    params << max;
+    Message* m = new Message(_fullId, "MaxValue", params.str());
+    UIManager::AddMessage(m);
+  }
+  else
+  {
+    if (max > 0)
+      MaxValue = max;
+  }
 }
 
 void ProgressBar::SetActualValue(f32 act)
 {
-	if (InvokeRequired())
-	{
-		stringstream params;
-		params << act;
-		Message* m = new Message(_fullId, "ActualValue", params.str());
-		UIManager::AddMessage(m);
-	}
-	else
-	{
-		if (act > MaxValue) act = MaxValue;
-		ActualValue = act;
-	}
+  if (InvokeRequired())
+  {
+    stringstream params;
+    params << act;
+    Message* m = new Message(_fullId, "ActualValue", params.str());
+    UIManager::AddMessage(m);
+  }
+  else
+  {
+    if (act > MaxValue)
+      act = MaxValue;
+    ActualValue = act;
+  }
 }
 
 void ProgressBar::ProcessMessage(Message& message)
 {
-	if (message.GetComponentId() != _fullId)
-	{
-		Control::ProcessMessage(message);
-		return;
-	}
+  if (message.GetComponentId() != _fullId)
+  {
+    Control::ProcessMessage(message);
+    return;
+  }
 
-	string tag = message.GetTag();
-	stringstream params(message.GetSerializedParameters());
+  string tag = message.GetTag();
+  stringstream params(message.GetSerializedParameters());
 
-	if (tag == "MaxValue")
-	{
-		f32 m;
-		params >> m;
-		SetMaxValue(m);
-	}
-	else if (tag == "ActualValue")
-	{
-		f32 a;
-		params >> a;
-		SetActualValue(a);
-	}
-	else if (tag == "DefaultColor")
-	{
-		u8 r, g, b, a;
-		params >> r >> g >> b >> a;
-		SetDefaultColor(Colors::FromRGBA(r, g, b, a));
-	}
-	else if (tag == "UsedColor")
-	{
-		u8 r, g, b, a;
-		params >> r >> g >> b >> a;
-		SetUsedColor(Colors::FromRGBA(r, g, b, a));
-	}
-	else
-		Control::ProcessMessage(message);
+  if (tag == "MaxValue")
+  {
+    f32 m;
+    params >> m;
+    SetMaxValue(m);
+  }
+  else if (tag == "ActualValue")
+  {
+    f32 a;
+    params >> a;
+    SetActualValue(a);
+  }
+  else if (tag == "DefaultColor")
+  {
+    u8 r, g, b, a;
+    params >> r >> g >> b >> a;
+    SetDefaultColor(Colors::FromRGBA(r, g, b, a));
+  }
+  else if (tag == "UsedColor")
+  {
+    u8 r, g, b, a;
+    params >> r >> g >> b >> a;
+    SetUsedColor(Colors::FromRGBA(r, g, b, a));
+  }
+  else
+    Control::ProcessMessage(message);
 }
 
 void ProgressBar::SetText(string s)
 {
-	l.Text(s);
+  l.Text(s);
 }
 
 void ProgressBar::SetValue(Object *sender, ProgressEventArgs *p)
 {
-	SetActualValue(p->percent);
-	l.Text(p->message);
+  SetActualValue(p->percent);
+  l.Text(p->message);
 }
 
 void ProgressBar::SetUsedColor(GXColor c)
 {
-	if (InvokeRequired())
-	{
-		stringstream params;
-		params << c.r << " " << c.g << " " << c.b << " " << c.a;
-		Message* m = new Message(_fullId, "UsedColor", params.str());
-		UIManager::AddMessage(m);
-	}
-	else
-	{
-		usedColor = c;
-	}
+  if (InvokeRequired())
+  {
+    stringstream params;
+    params << c.r << " " << c.g << " " << c.b << " " << c.a;
+    Message* m = new Message(_fullId, "UsedColor", params.str());
+    UIManager::AddMessage(m);
+  }
+  else
+  {
+    usedColor = c;
+  }
 }
 
 void ProgressBar::SetDefaultColor(GXColor c)
 {
-	if (InvokeRequired())
-	{
-		stringstream params;
-		params << c.r << " " << c.g << " " << c.b << " " << c.a;
-		Message* m = new Message(_fullId, "DefaultColor", params.str());
-		UIManager::AddMessage(m);
-	}
-	else
-	{
-		defaultColor = c;
-	}
+  if (InvokeRequired())
+  {
+    stringstream params;
+    params << c.r << " " << c.g << " " << c.b << " " << c.a;
+    Message* m = new Message(_fullId, "DefaultColor", params.str());
+    UIManager::AddMessage(m);
+  }
+  else
+  {
+    defaultColor = c;
+  }
 }
 
 void ProgressBar::Draw()
 {
-	Menu_DrawRectangle(GetLeft() + 1, GetTop() + 1, GetWidth() - 2, GetHeight()	- 2, defaultColor, 1);
-	Menu_DrawRectangle(GetLeft() + 1, GetTop() + 1, (f32)(GetWidth() - 2) * (ActualValue / MaxValue), GetHeight() - 2,	usedColor, 1);
-	Control::Draw();
+  Menu_DrawRectangle(GetLeft() + 1, GetTop() + 1, GetWidth() - 2, GetHeight() - 2, defaultColor, 1);
+  Menu_DrawRectangle(GetLeft() + 1, GetTop() + 1, (f32)(GetWidth() - 2) * (ActualValue / MaxValue), GetHeight() - 2, usedColor, 1);
+  Control::Draw();
 }
 
 void ProgressBar::ForeColor(GXColor c)
 {
-	l.ForeColor(c);
+  l.ForeColor(c);
 }
 
 void ProgressBar::TextSize(int pt)
 {
-	l.FontSize(pt);
+  l.FontSize(pt);
 }
 
 void ProgressBar::SetTextAlignement(HAlign halign, VAlign valign)
 {
-	l.HorizontalAlignement(halign);
-	l.VerticalAlignement(valign);
+  l.HorizontalAlignement(halign);
+  l.VerticalAlignement(valign);
 }

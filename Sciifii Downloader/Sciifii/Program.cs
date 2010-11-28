3f203a4bf6d5ed1c6a3694e04288eed612d2cb94
SkinnerDev <SkinnerDev@e6d814ab-5cd9-41c0-8def-334f96fd17b7>
2010-11-28 15:59:03 +0000

    if config.xml is v5 load new form else use old form.

--- Sciifii Downloader/Sciifii/Program.cs
+++ Sciifii Downloader/Sciifii/Program.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using System.IO;
+using SciifiiDTO;
+using System.Xml.Serialization;
 
 namespace Sciifii
 {
@@ -15,10 +18,38 @@ namespace Sciifii
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Use new form
-            Application.Run(new MainForm());
-            //Use old form
-            //Application.Run(new Form1());
+
+            String fileName = "config.xml";
+            
+            //Load config.xml
+            if (!File.Exists(fileName))
+            {
+                OpenFileDialog openFileDialog1 = new OpenFileDialog();
+                MessageBox.Show("File config.xml not found", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                    fileName = openFileDialog1.FileName;
+                else
+                {
+                    MessageBox.Show("config.xml is expected, application will close now", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+
+            try
+            {
+                SciifiiConfiguration datas = null;
+                using (Stream config = File.Open(fileName, FileMode.Open, FileAccess.Read))
+                    datas = (SciifiiConfiguration)new XmlSerializer(typeof(SciifiiConfiguration)).Deserialize(config);
+
+                switch (datas.Version)
+                {
+                    case 5:
+                        Application.Run(new MainFormV5(datas)); break;
+                    default:
+                        Application.Run(new MainFormV4(datas)); break;
+                }
+            }
+            catch
+            { /*Nothing to do just leave if filename empty*/ }
         }
     }
 }

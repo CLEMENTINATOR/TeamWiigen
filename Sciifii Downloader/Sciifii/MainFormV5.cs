60c8b360541f317d7b9691d73988a18b563510bc
SkinnerDev <SkinnerDev@e6d814ab-5cd9-41c0-8def-334f96fd17b7>
2010-11-28 15:57:46 +0000

    if config.xml is v5 load new form else use old form.

---
+++ Sciifii Downloader/Sciifii/MainFormV5.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using SciifiiDTO;
+
+namespace Sciifii
+{
+    public partial class MainFormV5 : Form
+    {
+        #region private
+        private SciifiiConfiguration datas;
+        #endregion
+
+        #region public
+        public MainFormV5(SciifiiConfiguration datas)
+        {
+            InitializeComponent();
+
+            this.datas = datas;
+            /*if (datas != null)
+                UpdateTextBox(tbStatus, "Config.xml load with success.");*/
+        }
+        #endregion
+
+        #region events
+        #endregion
+    }
+}

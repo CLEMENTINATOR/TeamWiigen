64c190199a07642577a6c82b23cde9239b2a9238
SkinnerDev <SkinnerDev@e6d814ab-5cd9-41c0-8def-334f96fd17b7>
2010-11-28 15:57:49 +0000

    if config.xml is v5 load new form else use old form.

---
+++ Sciifii Downloader/Sciifii/MainFormV5.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace Sciifii
+{
+    partial class MainFormV5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainFormV5));
+            this.SuspendLayout();
+            // 
+            // MainFormV5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainFormV5";
+            this.Text = "Sciifii downloader";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}

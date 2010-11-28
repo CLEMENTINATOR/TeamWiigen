88e668ebbbe5191e4107cb031b6c0ec3e3b96aaf
SkinnerDev <SkinnerDev@e6d814ab-5cd9-41c0-8def-334f96fd17b7>
2010-11-28 15:51:06 +0000

    update DTO to match with config.xml v5

---
+++ Sciifii Downloader/SciifiiDTO/Menu/ExitMenuItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace SciifiiDTO.Menu
+{
+    [Serializable]
+    [XmlType(TypeName = "exitMenuItem")]
+    public class ExitMenuItem
+    {
+        [XmlAttribute(AttributeName = "text")]
+        public String Text { get; set; }
+    }
+}
